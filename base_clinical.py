885d39168849a0f1e60e8f352dc20edef118e003
Masatoshi Abe <55616956+abebe9849@users.noreply.github.com>
2024-07-03 12:09:26 +0900

    Update base_clinical.py

--- base_clinical.py
+++ base_clinical.py
@@ -207,123 +207,16 @@ def get_transforms_1(*, data,CFG):
             Normalize(mean=[0.485, 0.456, 0.406],std=[0.229, 0.224, 0.225])
             ])
 
-def transform_qishen(*, data,CFG):
-    if data == 'train':
-        return Compose([
-            A.HorizontalFlip(p=0.5),
-            A.RandomBrightness(limit=0.2, p=0.75),
-            A.RandomContrast(limit=0.2, p=0.75),
-            A.OneOf([
-                A.OpticalDistortion(distort_limit=1.),
-                A.GridDistortion(num_steps=5, distort_limit=1.),
-                ], p=0.75),
-            A.HueSaturationValue(hue_shift_limit=40, sat_shift_limit=40, val_shift_limit=0, p=0.75),
-            A.ShiftScaleRotate(shift_limit=0.2, scale_limit=0.3, rotate_limit=30, border_mode=0, p=0.75),
-            A.CoarseDropout(max_holes=1, max_height=int(CFG.preprocess.size * 0.4), max_width=int(CFG.preprocess.size * 0.4), p=0.75),
-            #Normalize(mean=[0.485, 0.456, 0.406],std=[0.229, 0.224, 0.225])
-            ])
-    elif data == 'valid':
-        return Compose([
-            Resize(CFG.preprocess.size,CFG.preprocess.size),
-            #Normalize(mean=[0.485, 0.456, 0.406],std=[0.229, 0.224, 0.225])
-            ])
+
 
 #### augmentation ==============
 
-#### model ================
-#### model ================
-SEQ_POOLING = {
-    'gem': GeM(dim=2),
-    'concat': AdaptiveConcatPool2d(),
-    'avg': nn.AdaptiveAvgPool2d(1),
-    'max': nn.AdaptiveMaxPool2d(1)
-}
+ 
 
-class Model_iafoss(nn.Module):
-    def __init__(self, base_model='tf_efficientnet_b0_ns',pool="avg",pretrain=True):
-        super(Model_iafoss, self).__init__()
-        self.base_model = base_model 
-        if self.base_model=="dino_vit_s":
-            checkpoint_key = "teacher"
-            pretrained_weights = "/home/abebe9849/MAYO/dino/exp000/checkpoint0320.pth"
-            #self.model = vits.__dict__["vit_small"](patch_size=16, num_classes=0)
-            state_dict = torch.load(pretrained_weights, map_location="cpu")
-            if checkpoint_key is not None and checkpoint_key in state_dict:
-                print(f"Take key {checkpoint_key} in provided checkpoint dict")
-                state_dict = state_dict[checkpoint_key]
-            state_dict = {k.replace("module.", ""): v for k, v in state_dict.items()}
-            # remove `backbone.` prefix induced by multicrop wrapper
-            state_dict = {k.replace("backbone.", ""): v for k, v in state_dict.items()}
-            self.model.load_state_dict(state_dict, strict=False)
-            for _, p in self.model.named_parameters():
-                p.requires_grad = False
-            for _, p in self.model.head.named_parameters():
-                p.requires_grad = True
 
-            freeze =1
-            for n, p in self.model.blocks.named_parameters():
-                if int(n.split(".")[0])>=(12-freeze):
-                    p.requires_grad = True
-            avgpool_patchtokens = 0
-            n_last_blocks = 4
-            self.n_last_blocks = n_last_blocks
-            nc = self.model.embed_dim * (n_last_blocks + int(avgpool_patchtokens))
-            self.gru = nn.GRU(nc, 512, bidirectional=True, batch_first=True, num_layers=2)
 
-            nc*=64
-            self.head = nn.Sequential(nn.Linear(nc,512),
-                            nn.ReLU(), nn.Dropout(0.5),nn.Linear(512,1))
-            self.exam_predictor = nn.Linear(512*2, 1)
-            self.pool = nn.AdaptiveAvgPool1d(1)
-            
-        else:
-            self.model = timm.create_model(self.base_model, pretrained=True, num_classes=0,in_chans=3)
-            #self.model.conv_stem = nn.Conv2d(2, 32, kernel_size=3, padding=1, stride=1, bias=False)
-            nc = self.model.num_features
-            self.head = nn.Sequential(nn.AdaptiveAvgPool2d(1),nn.Flatten(),nn.Linear(nc,512),
-                            nn.ReLU(), nn.Dropout(0.5),nn.Linear(512,8))
-
-            self.gru = nn.GRU(nc, 512, bidirectional=True, batch_first=True, num_layers=2)
-            self.exam_predictor = nn.Linear(512*2, 8)
-            self.pool = nn.AdaptiveAvgPool1d(1)
-
-        
-    def forward(self, input1):
-        shape = input1.size()
-        batch_size = shape[0]
-        n = shape[1]
-        #print(input1.size())#torch.Size([2, 107, 3, 256, 256])
-        input1 = input1.reshape(-1,shape[2],shape[3],shape[4])
-
-        if "dino" in self.base_model:
-            intermediate_output = self.model.get_intermediate_layers(input1, self.n_last_blocks)
-            x = torch.cat([x[:, 0] for x in intermediate_output], dim=-1)
-            embeds, _ = self.gru(x.view(batch_size,n,x.shape[1]))
-            embeds = self.pool(embeds.permute(0,2,1))[:,:,0]
-            y = self.exam_predictor(embeds)
-            #x = x.view(batch_size,x.shape[1]*n)
-            #y = self.head(x)
-           
-            return y
-        else:
-            x = self.model.forward_features(input1) #bs*num_tile,embed_dim,h,w 
-            shape = x.size()
-            x = x.view(-1,n,shape[1],shape[2],shape[3]).permute(0,2,1,3,4).contiguous().view(-1,shape[1],shape[2]*n,shape[3])
-            y = self.head(x)
-            #x =  self.model(input1)
-            #embeds, _ = self.gru(x.view(batch_size,n,x.shape[1]))
-            #embeds = self.pool(embeds.permute(0,2,1))[:,:,0]
-            #y = self.exam_predictor(embeds)
-            return y
 
 #### model ================
-
-import vision_transformer as vits
-
-###  loss =============
-
-
-###  loss =============
 
 
 ###  metric =============
@@ -344,7 +237,6 @@ def AUC_(true,predict):
 
 def train_fn(CFG,fold,folds):
 
-    #nvnn_transform = A.load("/home/u094724e/NIH/siim2021_nvnn/pipeline1/configs/aug/s_0220/0220_hf_cut_sm2_0.75_384_v1.yaml", data_format='yaml')
 
     torch.cuda.set_device(CFG.general.device)
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -369,7 +261,6 @@ def train_fn(CFG,fold,folds):
     if CFG.model.name=="dino_vit_B":
         model = vits.__dict__["vit_base"](patch_size=16)
         pretrained_weights = "/home/abe/KidneyM/dino/pas_glomerulus_exp001/checkpoint0600.pth"
-        #pretrained_weights = "/home/abe/KidneyM/dino/pas_glomerulus_wbf_L/checkpoint0600.pth"
         state_dict = torch.load(pretrained_weights, map_location="cpu")["teacher"]
         state_dict = {k.replace("module.", ""): v for k, v in state_dict.items()}
         state_dict = {k.replace("backbone.", ""): v for k, v in state_dict.items()}
@@ -379,7 +270,6 @@ def train_fn(CFG,fold,folds):
     elif CFG.model.name=="dino_vit_s":
         model = vits.__dict__["vit_small"](patch_size=16)
         pretrained_weights = "/home/abe/KidneyM/dino/src/vit256_small_dino.pth"
-        #pretrained_weights = "/home/abe/KidneyM/dino/pas_glomerulus_wbf_L/checkpoint0600.pth"
         state_dict = torch.load(pretrained_weights, map_location="cpu")["teacher"]
         state_dict = {k.replace("module.", ""): v for k, v in state_dict.items()}
         state_dict = {k.replace("backbone.", ""): v for k, v in state_dict.items()}
@@ -388,8 +278,6 @@ def train_fn(CFG,fold,folds):
         model.head = nn.Linear(384*CFG.model.Ncat,n_target)
     elif CFG.model.name=="lunit":
         from timm.models.vision_transformer import VisionTransformer
-
-
         def get_pretrained_url(key):
             URL_PREFIX = "https://github.com/lunit-io/benchmark-ssl-pathology/releases/download/pretrained-weights"
             model_zoo_registry = {
@@ -549,13 +437,6 @@ def train_fn(CFG,fold,folds):
         AUC_score = roc_auc_score(valid_labels, preds[:,1])
 
         log.info(f'  Epoch {epoch+1} - avg_train_loss: {avg_loss:.6f}  avg_val_loss: {avg_val_loss:.5f} AUC_score {AUC_score:.4f}  time: {elapsed:.0f}s')
-
-        #if best_loss>avg_val_loss:#pr_auc best
-        #     best_loss = avg_val_loss
-            #best_preds = preds
-        #    log.info(f'  Epoch {epoch+1} - Save Best loss: {best_loss:.4f}')
-        #    torch.save(model.state_dict(), f'fold{fold}_{CFG.general.exp_num}_best_loss.pth')
-
         if AUC_score>best_score:#pr_auc best
             best_score = AUC_score
             best_preds = preds
@@ -610,8 +491,7 @@ def submit(test_df,folds,CFG):
     for fold in range(folds["fold"].nunique()):
         if CFG.model.name=="dino_vit_B":
             model = vits.__dict__["vit_base"](patch_size=16)
-            pretrained_weights = "/home/abe/KidneyM/dino/pas_glomerulus_exp001/checkpoint0600.pth"
-            #pretrained_weights = "/home/abe/KidneyM/dino/pas_glomerulus_wbf_L/checkpoint0600.pth"
+            pretrained_weights = "checkpoint0600.pth"
             state_dict = torch.load(pretrained_weights, map_location="cpu")["teacher"]
             state_dict = {k.replace("module.", ""): v for k, v in state_dict.items()}
             state_dict = {k.replace("backbone.", ""): v for k, v in state_dict.items()}
@@ -661,9 +541,6 @@ def submit(test_df,folds,CFG):
         else:
             model = timm.create_model(CFG.model.name, pretrained=True,num_classes=n_target,in_chans=3) 
         model.load_state_dict(torch.load(f'fold{fold}_{CFG.general.exp_num}_best_AUC.pth'),strict=False)
-
-        #model.load_state_dict(torch.load(f'/home/abe/KidneyM/dino/src/outputs/2023-09-28/14-16-10/fold{fold}_{CFG.general.exp_num}_best_AUC.pth'))
-
         model.to(device)
         
         preds = eval_func(model, valid_loader, device,CFG)
@@ -688,8 +565,7 @@ def submit(test_df,folds,CFG):
 
 
 log = logging.getLogger(__name__)
-@hydra.main(config_path="/home/abe/KidneyM/dino/src/",config_name="base")
-#@hydra.main(config_path="/home/abe/KidneyM/dino/src/outputs/2023-11-27/09-08-47/.hydra/",config_name="config")
+@hydra.main(config_path="./",config_name="base")
 def main(CFG : DictConfig) -> None:
 
     CFG.general.exp_num+="_"+CFG.task+"_"
@@ -704,30 +580,8 @@ def main(CFG : DictConfig) -> None:
     #os.environ["CUDA_VISIBLE_DEVICES"]=f"{CFG.general.device}"
     log.info(f"===============exp_num{CFG.general.exp_num}============")
     
-    #if CFG.task =="alb":
-    #
-    #    folds = pd.read_csv("/home/abe/KidneyM/dino/FIX/B600__Alb_over3.0/oof.csv")
-    #    test_df = pd.read_csv("/home/abe/KidneyM/dino/FIX/B600__Alb_over3.0/sub.csv")
-    #elif CFG.task =="dbp":
-    #    folds = pd.read_csv("/home/abe/KidneyM/dino/FIX/B600__dBP_over90/oof.csv")
-    #    test_df = pd.read_csv("/home/abe/KidneyM/dino/FIX/B600__dBP_over90/sub.csv")
-    #elif CFG.task =="DM":
-    #    folds = pd.read_csv("/home/abe/KidneyM/dino/FIX/B600__DM_seed2021/oof.csv")
-    #    test_df = pd.read_csv("/home/abe/KidneyM/dino/FIX/B600__DM_seed2021/sub.csv")
-    #elif CFG.task =="sbp":
-    #    folds = pd.read_csv("/home/abe/KidneyM/dino/FIX/B600__sBP_seed2021_over140/oof.csv")
-    #    test_df = pd.read_csv("/home/abe/KidneyM/dino/FIX/B600__sBP_seed2021_over140/sub.csv")
-    #elif CFG.task =="hep":
-    #    folds = pd.read_csv("/home/abe/KidneyM/dino/FIX/B600__血尿_over1/oof.csv")
-    #    test_df = pd.read_csv("/home/abe/KidneyM/dino/FIX/B600__血尿_over1/sub.csv")
-    #elif CFG.task =="egfr":
-    #    folds = pd.read_csv("/home/abe/KidneyM/dino/FIX/B600__eGFR_over60/oof.csv")
-    #    test_df = pd.read_csv("/home/abe/KidneyM/dino/FIX/B600__eGFR_over60/sub.csv")
-    #elif CFG.task =="upc":
-    #    folds = pd.read_csv("/home/abe/KidneyM/dino/FIX/B600__UPC_seed2023_over3.5/oof.csv")
-    #    test_df = pd.read_csv("/home/abe/KidneyM/dino/FIX/B600__UPC_seed2023_over3.5/sub.csv")
 
-    all_df = pd.read_csv("/home/abe/KidneyM/data/final_2_pas.csv")
+    all_df = pd.read_csv("final_2_pas.csv")
     dic_ = {"egfr":60,"alb":3,"uprot":3.5,"DM":1,"DBP":90,"収縮期血圧":140,"血尿":1}
     COL = CFG.task
     CONF = dic_[COL]
